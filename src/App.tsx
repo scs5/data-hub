43353785267dd9230f7ce964a6079640295ba390
Sam Smith <scs5@clemson.edu>
2025-08-07 08:19:20 -0400

    feat: remove '#' from page URLs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Music from './pages/Music';
 import Fitness from './pages/Fitness';
 import Health from './pages/Health';
